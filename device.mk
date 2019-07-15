PixelExperience-Devices/device_xiaomi_rosy

rosy: Migrate to livedisplay 2.0
Change-Id: I513c284c534ce3abc27bf374513280171454f4d1

device.mk
@@ -254,9 +254,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_rosy
 
-# LiveDisplay native
+# LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \

TheXPerienceProject/android_device_xiaomi_rosy

device.mk
@@ -280,9 +280,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_rosy
 
-# LiveDisplay native
+# LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.mokee.livedisplay@1.0-service-qdcm
+    vendor.mokee.livedisplay@2.0-service-sdm
 
 # Netutils
 PRODUCT_PACKAGES += \